5dc1e7f1810ce3a2a1fa1279755abf2699231095
prefer_youn <38209666+shyoun94@users.noreply.github.com>
2024-03-04 18:56:02 +0900

    first commit

---
+++ Router_tutorial/src/main.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./ErrorPage/ErrorPage";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import Edit, { action as editAction } from "./routes/edit";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <Edit />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
